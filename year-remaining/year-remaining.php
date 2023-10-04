6def30b41365a5fe4852ff4f245f42f60583582a
admin <andrewhoyer@gmail.com>
2023-10-04 11:17:54 -0700

    GPL-3.0 license text

--- year-remaining/year-remaining.php
+++ year-remaining/year-remaining.php
@@ -15,6 +15,24 @@
  * Requires PHP:      7.0
  */
 
+/*
+	Year Remaining - Outputs the percentage of time remaining in the year.
+	Copyright (C) 2023 Andrew Hoyer
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 function yr_year_remaining($atts) {
 	return yr_generate();
 }
